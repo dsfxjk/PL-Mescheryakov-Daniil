fc52d861fbfd29a44f33ed2b09380d8b55953fef
Даниил <dsfxjk1331@mail.ru>
2025-09-26 17:28:05 +0300

    .

--- 4.10.py
+++ 4.10.py
@@ -2,10 +2,13 @@ n = int(input())
 k = int(input())
 a = 1
 b = 1
+position = 1
 sum = 0
-for i in range(k+n-1):
-    if i >= k-1:
+while n>0:
+    if position>=k:
         sum += a
+        n -= 1
     a = b
     b = a+b
+    position += 1
 print(sum)
