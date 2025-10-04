766302864e4f0836d52e61b3c28e0234cfadcd8c
Даниил <dsfxjk1331@mail.ru>
2025-10-04 13:30:33 +0300

    .

--- 6.5.1.py
+++ 6.5.1.py
@@ -3,6 +3,6 @@ spisok = list(map(int, input("Введите 10 чисел через пробе
 if len(spisok) != 10:
     print("Надо ввести 10 чисел")
 else:
-    for i in range(len(spisok)-1):
-        if spisok[i]<0  and spisok[i+1]<0:
+    for i in range(len(spisok) - 1):
+        if spisok[i] < 0  and spisok[i+1] < 0:
             print(spisok[i], spisok [i+1])
