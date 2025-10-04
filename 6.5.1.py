78be1a0a1acb88b2d288ec9791e81226063eead9
Даниил <dsfxjk1331@mail.ru>
2025-10-04 13:26:43 +0300

    .

---
+++ 6.5.1.py
@@ -0,0 +1,8 @@
+spisok = list(map(int, input("Введите 10 чисел через пробел: ").split()))
+
+if len(spisok) != 10:
+    print("Надо ввести 10 чисел")
+else:
+    for i in range(len(spisok)-1):
+        if spisok[i]<0  and spisok[i+1]<0:
+            print(spisok[i], spisok [i+1])
