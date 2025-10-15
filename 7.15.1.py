021e6baaa59dbdf8d6b26419a444b201b6e42748
Даниил <dsfxjk1331@mail.ru>
2025-10-15 14:26:14 +0300

    .

--- 7.15.1.py
+++ 7.15.1.py
@@ -1,12 +1,12 @@
 def prost(n):
     if n<2:
         return False
-    if n==2:
+    if n == 2:
         return True
     if n%2 == 0:
         return False
     for i in range(3, int(n**0.5)+1, 2):
-        if n % i == 0:
+        if n%i == 0:
             return False
     return True
 
